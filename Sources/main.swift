aa47bedd25c9cc3f12ab26460ac23d38eeb4b374
Kyle Jessup <badthing@gmail.com>
2016-07-12 14:26:51 -0400

    Added command line arguments for more thorough configuration options.

--- Sources/main.swift
+++ Sources/main.swift
@@ -24,11 +24,6 @@ import PerfectHTTPServer
 // Initialize base-level services
 PerfectServer.initializeServices()
 
-// Create our webroot
-// This will serve all static content by default
-let webRoot = "./webroot"
-try Dir(webRoot).create()
-
 // Add our routes and such
 // Register your own routes and handlers
 Routing.Routes["/"] = {
@@ -39,9 +34,22 @@ Routing.Routes["/"] = {
 }
 
 do {
-    
-    // Launch the HTTP server on port 8181
-    try HTTPServer(documentRoot: webRoot).start(port: 8181)
+    let server = HTTPServer()
+	
+	// Set a listen port of 8181
+	server.serverPort = 8181
+	
+	// Set a document root.
+	// This is optional. If you do not want to serve static content then do not set this.
+	server.documentRoot = "./webroot"
+	
+	// Gather command line options and further configure the server.
+	// Run the server with --help to see the list of supported arguments.
+	// Any command line arguments will supplant any of the values set above.
+	configureServer(server)
+	
+    // Launch the HTTP server.
+    try server.start()
     
 } catch PerfectError.networkError(let err, let msg) {
     print("Network error thrown: \(err) \(msg)")
