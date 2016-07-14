d4ee50c42eb08f79c9391abfda8796266cf4f533
Kyle Jessup <badthing@gmail.com>
2016-07-14 15:23:06 -0400

    Updated for new routes api

--- Sources/main.swift
+++ Sources/main.swift
@@ -21,33 +21,38 @@ import PerfectLib
 import PerfectHTTP
 import PerfectHTTPServer
 
-// Add our routes and such
-// Register your own routes and handlers
-Routing.Routes["/"] = {
-	request, response in
+// Create HTTP server.
+let server = HTTPServer()
 
-	response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
-	response.completed()
-}
+// Register your own routes and handlers
+var routes = Routes()
+routes.add(method: .get, uri: "/", handler: {
+		request, response in
+
+		response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+		response.completed()
+	}
+)
+
+// Add the routes to the server.
+server.addRoutes(routes)
+
+// Set a listen port of 8181
+server.serverPort = 8181
+
+// Set a document root.
+// This is optional. If you do not want to serve static content then do not set this.
+// Setting the document root will automatically add a static file handler for the route /**
+server.documentRoot = "./webroot"
+
+// Gather command line options and further configure the server.
+// Run the server with --help to see the list of supported arguments.
+// Command line arguments will supplant any of the values set above.
+configureServer(server)
 
 do {
-	let server = HTTPServer()
-	
-	// Set a listen port of 8181
-	server.serverPort = 8181
-	
-	// Set a document root.
-	// This is optional. If you do not want to serve static content then do not set this.
-	server.documentRoot = "./webroot"
-	
-	// Gather command line options and further configure the server.
-	// Run the server with --help to see the list of supported arguments.
-	// Command line arguments will supplant any of the values set above.
-	configureServer(server)
-	
 	// Launch the HTTP server.
 	try server.start()
-    
 } catch PerfectError.networkError(let err, let msg) {
 	print("Network error thrown: \(err) \(msg)")
 }
