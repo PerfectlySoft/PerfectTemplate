5698c3978260ce9e68fbfd35c0d0632f8fc039fc
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-09-09 22:01:29 -0400

    Added CRUD/SQLite dependency

--- Package.swift
+++ Package.swift
@@ -9,8 +9,9 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", from: "3.0.0"),
 	],
 	targets: [
-		.target(name: "PerfectTemplate", dependencies: ["PerfectHTTPServer"])
+		.target(name: "PerfectTemplate", dependencies: ["PerfectHTTPServer", "PerfectSQLite"])
 	]
 )
